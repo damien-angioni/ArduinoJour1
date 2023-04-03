f4cf56844b01c9340a58095f9325cb76b02d1414
damien-angioni <damien.angioni@laplateforme.io>
2023-04-03 15:54:31 +0200

    job10
    -Proposition pour le job 10

---
+++ job10/job10.ino
@@ -0,0 +1,60 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogWrite(9, 0);
+delay(1000);
+analogWrite(9, 10);
+delay(100);
+analogWrite(9, 20);
+delay(100);
+analogWrite(9, 30);
+delay(100);
+analogWrite(9, 40);
+delay(100);
+analogWrite(9, 50);
+delay(100);
+analogWrite(9, 60);
+delay(100);
+analogWrite(9, 70);
+delay(100);
+analogWrite(9, 80);
+delay(100);
+analogWrite(9, 90);
+delay(100);
+analogWrite(9, 100);
+delay(100);
+analogWrite(9, 110);
+delay(100);
+analogWrite(9, 120);
+delay(100);
+analogWrite(9, 130);
+delay(100);
+analogWrite(9, 140);
+delay(100);
+analogWrite(9, 150);
+delay(100);
+analogWrite(9, 160);
+delay(100);
+analogWrite(9, 170);
+delay(100);
+analogWrite(9, 180);
+delay(100);
+analogWrite(9, 190);
+delay(100);
+analogWrite(9, 200);
+delay(100);
+analogWrite(9, 210);
+delay(100);
+analogWrite(9, 220);
+delay(100);
+analogWrite(9, 230);
+delay(100);
+analogWrite(9, 240);
+delay(100);
+analogWrite(9, 250);
+delay(1000);
+}
