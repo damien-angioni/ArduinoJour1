6019a311b91afb1e25ede5b728b7ae99f027aebd
damien-angioni <damien.angioni@laplateforme.io>
2023-04-03 15:19:13 +0200

    job09
    Proposition job 09

---
+++ job09/job09.ino
@@ -0,0 +1,53 @@
+
+char nique="Hello Fils de pute, je te hais profondément";
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(12,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(7, HIGH);
+  delay(200);
+  digitalWrite(7, LOW);
+  delay(150);
+  digitalWrite(8, HIGH);
+  delay(200);
+  digitalWrite(8, LOW);
+  delay(200);
+  digitalWrite(9, HIGH);
+  delay(200);
+  digitalWrite(9, LOW);
+  delay(200);
+  digitalWrite(10, HIGH);
+  delay(200);
+  digitalWrite(10, LOW);
+  delay(200);
+  digitalWrite(11, HIGH);
+  delay(200);
+  digitalWrite(11, LOW);
+  delay(200);
+  digitalWrite(12, HIGH);
+  delay(200);
+  digitalWrite(12, LOW);
+  delay(200);
+  digitalWrite(6, HIGH);
+  delay(200);
+  digitalWrite(6, LOW);
+  delay(200);
+  digitalWrite(5, HIGH);
+  delay(200);
+  digitalWrite(5, LOW);
+  delay(450);
+
+printf(nique);
+}
