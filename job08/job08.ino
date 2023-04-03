0b4d98cfc4b3bbede8ba6b5ba8dc4ded097b0e4f
damien-angioni <damien.angioni@laplateforme.io>
2023-04-03 14:36:56 +0200

    job 8
    -Illumination asynchrone des deux diodes

---
+++ job08/job08.ino
@@ -0,0 +1,23 @@
+
+char nique="Hello Fils de pute, je te hais profondément";
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(7, HIGH);
+  delay(200);
+  digitalWrite(7, LOW);
+  delay(150);
+  digitalWrite(8, HIGH);
+  delay(200);
+  digitalWrite(8, LOW);
+  delay(450);
+
+printf(nique);
+}
